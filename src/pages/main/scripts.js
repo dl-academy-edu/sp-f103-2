9bf065839cef9ad39d532eb8d7e39049d67aae20
Sergey Pechinov <sen1c163rus@gmail.com>
2020-08-18 20:58:33 +0400

    Lesson 6 complete

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,15 +1,16 @@
 var popup = document.querySelector(".modal_js"),
     buttonOpen = document.querySelector(".button-open_js"),
     buttonClose = document.querySelector(".button-close_js"),
-    input = document.querySelector(".input-text_js");
+    input = document.querySelector(".input-text_js"),
+    html = document.querySelector("html");
 
 buttonOpen.addEventListener("click", function() {
-    popup.classList.add("modal-bg_open");
+    html.classList.add("modal-bg_open");
     input.focus();
 });
 
 buttonClose.addEventListener("click", function() {
-    popup.classList.remove("modal-bg_open");
+    html.classList.remove("modal-bg_open");
     buttonOpen.focus();
 });
 
@@ -18,4 +19,15 @@ window.addEventListener("keydown", function(e) {
     if(e.code === "Escape" && popup.classList.contains("modal-bg_open")) {
         popup.classList.remove("modal-bg_open");
     }
-})
+});
+
+var buttonOpenMenu = document.getElementById("js-button-open-menu"),
+    buttonCloseMenu = document.getElementById("js-button-close-menu");
+
+buttonOpenMenu.addEventListener("click", function () {
+    html.classList.add("open-mobile-menu");
+});
+
+buttonCloseMenu.addEventListener("click", function () {
+    html.classList.remove("open-mobile-menu");
+});
